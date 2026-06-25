623789f89522c1ab1a314ca931e7f0f80ce02c81
nmtm6010 <ytnmtm6010@gmail.com>
2026-06-25 19:56:00 +0000

    collapsible sidebar

--- components/layout/sidebar.tsx
+++ components/layout/sidebar.tsx
@@ -3,6 +3,7 @@
   import Link from "next/link"; 
   import { usePathname } from "next/navigation"; 
   import { sidebarlinks } from "@/lib/sidebarlinks";
+  import {ChevronRight, ChevronLeft} from "lucide-react";
    export default function Sidebar() {
      const pathname = usePathname(); 
      const [collapsed, setCollapsed] = useState(false); 
@@ -15,14 +16,20 @@
         else if (pathname.startsWith("/settings")) { links = sidebarlinks.settings; } 
         else if (pathname === "/") { links = sidebarlinks.home; } 
         return (
-           <aside className= "w-80 border-r min-h-screen bg-gray-100 transition-all duration-300}"> 
+           <aside className= {` ${collapsed ? "w-20" : "w-80"} border-r min-h-screen bg-gray-100 transition-all duration-300`}> 
            <nav className="flex flex-col"> 
             {links.map((link) => 
             { const Icon = link.icon; return (
                <Link key={link.href} href={link.href} className="block w-full text-green-700 font-bold border-b-3 border-b-gray-400/60 py-5 pb-3 pl-8 text-xl hover:bg-gray-200 transition-colors flex items-center gap-3" > 
-               <Icon size={22} /> {} 
-               {link.name} 
-               </Link> ); 
-               })}
+               <Icon size={22} />{}
+               {!collapsed && link.name}
+               </Link>
+               )})}
+            <button
+  onClick={() => setCollapsed(!collapsed)}
+  className="block w-full text-green-700 font-bold border-b-3 border-b-gray-400/60 py-5 pb-3 pl-8 text-xl hover:bg-gray-200 transition-colors flex items-center gap-3"
+>
+  {collapsed ? <ChevronRight size="22"/> : <ChevronLeft size="22"/> }
+</button>
         </nav> 
     </aside> ); }
